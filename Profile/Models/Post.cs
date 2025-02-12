b8c096d2b27cb59646b370710b2ed7616638f4ce
Kelium Jojoa Urrutia <keliumju@gmail.com>
2025-02-11 23:21:53 -0500

    Add the post model

---
+++ Profile/Models/Post.cs
@@ -0,0 +1,24 @@
+namespace Profile.Models;
+
+public class Post {
+  public String? Title {get; set;}
+  public String? Description {get; set;}
+  public String? Author {get; set;}
+  public String? Date {get; set;}
+  public String? Slug {get; set;}
+
+  public String? Content {get; set;}
+
+  public String GetUrl() {
+    return $"/dsa/{Slug}";
+  }
+
+  public String GetFormattedDate() {
+    return GetDateWithType().ToString("MMMM, dd, yyy");
+  }
+
+  public DateTime GetDateWithType() {
+    return DateTime.Parse(Date);
+  }
+}
+
